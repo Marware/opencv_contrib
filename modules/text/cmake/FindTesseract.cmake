a5d542d915747d1ec0146cb558ac7ebbf75b854f
Marwan Yassin <marwan.yassin@gmail.com>
2016-12-07 23:44:13 +0200

    Fix libtesseract with OpenCL linking errors

--- modules/text/cmake/FindTesseract.cmake
+++ modules/text/cmake/FindTesseract.cmake
@@ -1,6 +1,6 @@
 # Tesseract OCR
 if(COMMAND pkg_check_modules)
-  pkg_check_modules(Tesseract tesseract lept)
+  pkg_check_modules(Tesseract tesseract lept OpenCL)
 endif()
 if(NOT Tesseract_FOUND)
   find_path(Tesseract_INCLUDE_DIR tesseract/baseapi.h
@@ -14,10 +14,14 @@ if(NOT Tesseract_FOUND)
   find_library(Lept_LIBRARY NAMES lept
     HINTS
     /usr/local/lib)
+    
+  find_library(OpenCL_LIBRARY NAMES OpenCL
+    HINTS
+    /usr/local/lib)  
 
   if(Tesseract_INCLUDE_DIR AND Tesseract_LIBRARY AND Lept_LIBRARY)
     set(Tesseract_INCLUDE_DIRS ${Tesseract_INCLUDE_DIR})
-    set(Tesseract_LIBRARIES ${Tesseract_LIBRARY} ${Lept_LIBRARY})
+    set(Tesseract_LIBRARIES ${Tesseract_LIBRARY} ${Lept_LIBRARY} ${OpenCL_LIBRARY})
     set(Tesseract_FOUND 1)
   endif()
 endif()
